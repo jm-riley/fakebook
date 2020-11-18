929ae7b59b8954d8b97491ab83b4ea0fd22f76c1
John-Michael Riley <johnmichael.w.riley@gmail.com>
2020-11-18 13:39:17 -0500

    add user form / csrf middleware

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
 const postsRouter = require('./routes/posts');
 const { User, Post } = require('./models/index.js');
 
 app.set('view engine', 'pug');
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const csrfProtection = csrf({ cookie: true });
+
+function logReqData(req, res, next) {
+  console.log('HTTP METHOD:', req.method);
+  console.log('PATH:', req.path);
+  next();
+}
+
+function addToReq(req, res, next) {
+  if (req.body.username === 'fred') {
+    req.isFred = true;
+  } else {
+    req.isFred = false;
+  }
+  next();
+}
+
+app.use(logReqData);
+app.use(addToReq);
 
 app.use('/posts', postsRouter);
 
@@ -12,6 +37,21 @@ app.get('/', async (req, res) => {
   const users = await User.findAll();
   res.render('index', { title: 'Fakebook', users });
   // res.send('<h1>this is our express app</h1>');
+});
+
+app.get('/register', csrfProtection, (req, res) => {
+  console.log(req.csrfToken());
+  res.render('new-user', { csrfToken: req.csrfToken() });
+});
+
+app.post('/users', csrfProtection, async (req, res) => {
+  console.log(req.body);
+  const { username, email } = req.body;
+  const user = await User.create({ username, email });
+  if (req.isFred) {
+    return res.send('fred just registered');
+  }
+  res.redirect('/');
 });
 
 app.get('/users/:id(\\d+)', async (req, res) => {
@@ -24,10 +64,10 @@ app.get('/test', (req, res) => {
   res.send('this is the test route');
 });
 
-app.get('/:state/:city', (req, res) => {
-  console.log(req.params);
-  res.send(`state: ${req.params.state}, city: ${req.params.city}`);
-});
+// app.get('/:state/:city', (req, res) => {
+//   console.log(req.params);
+//   res.send(`state: ${req.params.state}, city: ${req.params.city}`);
+// });
 
 app.all('*', (req, res) => {
   res.statusCode = 404;
